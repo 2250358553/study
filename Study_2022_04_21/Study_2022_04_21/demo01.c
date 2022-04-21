a4b8cf4ea356ed0c8d8838aa546279fd8b14a776
ICCC <55134689+2250358553@users.noreply.github.com>
2022-04-21 23:29:11 +0800

    Add files via upload

---
+++ Study_2022_04_21/Study_2022_04_21/demo01.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+//Á½¶ËÍùÖĞ¼äÌæ»»
+//int main()
+//{
+//	char arr1[] = "hello world";
+//	char arr2[] = "***********";
+//	int left = 0;
+//	int right = strlen(arr1) - 1;
+//	printf("%s\n", arr2);
+//	while (left <= right)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		printf("%s\n", arr2);
+//		Sleep(1000);//Ë¯Ãß1s
+//		System("cls");//Çå¿ÕÆÁÄ»
+//		left++;
+//		right--;
+//	}
+//
+//	return 0;
+//}
+
+
+int main()
+{
+
+	return 0;
+}
